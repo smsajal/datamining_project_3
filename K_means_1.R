65cdc198ca2e68daca42cb8f6b2f19f2dfb2b69d
Sultan Mahmud Sajal <sxs2561@psu.edu>
2019-04-19 17:20:22 -0400

    k-means done-1

--- K_means_1.R
+++ K_means_1.R
@@ -4,6 +4,7 @@ library("mclust")
 rm(list=ls())
 setwd("/Users/sxs2561/Documents/AcademicAssignments/stat_557/project_3/datamining_project_3/data")
 
+set.seed(2)
 #### READ DATA
 ########### Read data ##########
 data.test=read.csv("testing.csv")
@@ -17,7 +18,16 @@ x.wf <- data[which(as.factor(data$class) == ("water") | as.factor(data$class) ==
 y.wf <- x.wf$class
 y.wf<-as.vector(y.wf)
 x.wf <- subset(x.wf , select = -class)
+is.data.frame(x.wf)
 
+
+### making random initialization
+numberOfClusters=2
+centerIndexes=sample(1:length(x.wf),numberOfClusters,replace = FALSE)
+print(centerIndexes)
+centers=x.wf[centerIndexes,c(0:length(x.wf))]
 #### APPLIED KMEANS CLUSTERING WITH 2 CLUSTERS
-km.out=kmeans(x = x.wf,2)
+set.seed(7)
+km.out=kmeans(x = x.wf,centers=centers)
+
 print(adjustedRandIndex(y.wf,km.out$cluster))
