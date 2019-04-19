76eaf196d5e76deb62b38380d7f285cb0ee9595c
Sultan Mahmud Sajal <sxs2561@psu.edu>
2019-04-19 18:31:28 -0400

    kmeans done-2

--- K_means_1.R
+++ K_means_1.R
@@ -4,7 +4,7 @@ library("mclust")
 rm(list=ls())
 setwd("/Users/sxs2561/Documents/AcademicAssignments/stat_557/project_3/datamining_project_3/data")
 
-set.seed(2)
+set.seed(391)
 #### READ DATA
 ########### Read data ##########
 data.test=read.csv("testing.csv")
@@ -22,12 +22,12 @@ is.data.frame(x.wf)
 
 
 ### making random initialization
-numberOfClusters=2
-centerIndexes=sample(1:length(x.wf),numberOfClusters,replace = FALSE)
+numberOfClusters=6
+centerIndexes=sample(1:nrow(x.wf),numberOfClusters,replace = FALSE)
 print(centerIndexes)
 centers=x.wf[centerIndexes,c(0:length(x.wf))]
 #### APPLIED KMEANS CLUSTERING WITH 2 CLUSTERS
-set.seed(7)
+# set.seed(7)
 km.out=kmeans(x = x.wf,centers=centers)
 
 print(adjustedRandIndex(y.wf,km.out$cluster))
