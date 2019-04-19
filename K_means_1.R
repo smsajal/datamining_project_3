49ab3686efc0171040c135cea69f04ecbb1f5846
Sultan Mahmud Sajal <sxs2561@psu.edu>
2019-04-19 12:29:51 -0400

    initial kmeans implementation

--- K_means_1.R
+++ K_means_1.R
@@ -1,24 +1,23 @@
 library('utils')
 library("clues")
+library("mclust")
 rm(list=ls())
 setwd("/Users/sxs2561/Documents/AcademicAssignments/stat_557/project_3/datamining_project_3/data")
 
 #### READ DATA
-data.test=read.csv("testing_k_final.csv")
-data.train=read.csv("training_k_final.csv")
-data=read.csv("training_k_final.csv")
+########### Read data ##########
+data.test=read.csv("testing.csv")
+data.train=read.csv("training.csv")
+data=read.csv("training.csv")
 
 x <- subset(data, select=-class)
 y <- data$class
 
-# x<- scale(x)
+x.wf <- data[which(as.factor(data$class) == ("water") | as.factor(data$class) == ("farm")),]
+y.wf <- x.wf$class
+y.wf<-as.vector(y.wf)
+x.wf <- subset(x.wf , select = -class)
 
-
-print(length(y))
-km.out=kmeans(x = x,6)
-print(length(km.out$cluster))
-
-
-print(adjustedRand(y,km.out$cluster))
-# print(km.out)
-plot(x=x$X20150720_N,y=x$X20150226_N,col=(km.out$cluster+1),pch=20,cex=2)
+#### APPLIED KMEANS CLUSTERING WITH 2 CLUSTERS
+km.out=kmeans(x = x.wf,2)
+print(adjustedRandIndex(y.wf,km.out$cluster))
