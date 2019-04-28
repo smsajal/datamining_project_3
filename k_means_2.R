801650801ee8442cba966d628c0a0fbefb13b902
Sultan Mahmud Sajal <sxs2561@psu.edu>
2019-04-28 13:16:36 -0400

    added graph of cluster

--- k_means_2.R
+++ k_means_2.R
@@ -1,6 +1,8 @@
 library('utils')
 library("clues")
 library("mclust")
+library("useful")
+library("factoextra")
 rm(list = ls())
 setwd(
     "/Users/sxs2561/Documents/AcademicAssignments/stat_557/project_3/datamining_project_3/data"
@@ -37,19 +39,19 @@ print(dim(data)) #5743   26
 #      type="b", pch = 19, frame = FALSE,
 #      xlab="Number of clusters K",
 #      ylab="Total within-clusters sum of squares")
-aRandIdx=c()
-for (i in 1:100) {
-    set.seed(i)
-    numberOfClusters = 9
-    centerIndexes = sample(1:nrow(x.wf), numberOfClusters, replace = FALSE)
-    # print(centerIndexes)
-    centers = x.wf[centerIndexes, c(0:length(x.wf))]
-    #### APPLIED KMEANS CLUSTERING WITH 2 CLUSTERS
-    # set.seed(7)
-    km.out = kmeans(x = x.wf, centers = centers)
-    
-    # print(adjustedRandIndex(y.wf, km.out$cluster))
-    aRandIdx[i]<-adjustedRandIndex(y.wf,km.out$cluster)
-}
-plot(x=c(1:length(aRandIdx)),y=aRandIdx,main = "Effect of Random Initialization on k-means performance for k=9",xlab = "Seed",ylab = "Adjusted Rand Index",pch=18,col='blue',cex=2.2,cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
-print(mean(aRandIdx))
+#aRandIdx=c()
+#for (i in 1:100) {
+set.seed(3)
+numberOfClusters = 3
+centerIndexes = sample(1:nrow(x.wf), numberOfClusters, replace = FALSE)
+centers = x.wf[centerIndexes, c(0:length(x.wf))]
+km.out = kmeans(x = x.wf, centers = centers)
+
+fviz_cluster(km.out,data=x.wf)
+?fviz_cluster()
+
+print('hello')    
+    #aRandIdx[i]<-adjustedRandIndex(y.wf,km.out$cluster)
+#}
+#plot(x=c(1:length(aRandIdx)),y=aRandIdx,main = "Effect of Random Initialization on k-means performance for k=9",xlab = "Seed",ylab = "Adjusted Rand Index",pch=18,col='blue',cex=2.2,cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+#print(mean(aRandIdx))
